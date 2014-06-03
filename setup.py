2c9f1dade62eba851a160160e9877c0c8e724e2d
encorehu <huyoo353@126.com>
2014-06-03 21:49:04 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+        name = "nlp",
+        version="0.1.0",
+        packages = find_packages(),
+        zip_safe = False,
+
+        description = "python nlp lib, for fun.",
+        long_description = "python nlp lib, for fun.",
+        author = "encorehu",
+        author_email = "huyoo353@126.com",
+
+        license = "MIT",
+        keywords = ("nlp", "egg"),
+        platforms = "all",
+        url = "https://github.com/encorehu/nlp",
+)
