ed75c72a0129ffbf487881711171044510829852
encorehu <huyoo353@126.com>
2014-06-04 21:24:30 +0800

    add find_between function, between two string

--- nlp/extractors/base.py
+++ nlp/extractors/base.py
@@ -6,6 +6,21 @@ class BaseExtractor(object):
         result =[]
         return result
 
+    def find_between(self, text, s1, s2=None):
+        if not s1:
+            raise Exception('s1 is None!')
+
+        pos1 = text.find(s1)
+        if s2:
+            pos2 = text.find(s2)
+        else:
+            pos2 = -1
+
+        if pos2 != -1 and pos2>pos1:
+            return text[pos1+len(s1):pos2]
+        else:
+            return ''
+
     def extract(self, html):
         return self._extract(html)
 
